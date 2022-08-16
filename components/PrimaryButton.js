209377144174246e59c68f25fe89cce6d219acdd
Mohammad Arif <arif_iq@yahoo.co.in>
2022-08-16 20:34:28 +0530

    Initial commit

---
+++ components/PrimaryButton.js
@@ -0,0 +1,31 @@
+import { View, Text, Pressable, StyleSheet } from 'react-native'
+
+function PrimaryButton ({ children }) {
+  function pressHandler () {
+    console.log('Pressed')
+  }
+  return (
+    <Pressable onPress={pressHandler} android_ripple={{ color: '#640233' }}>
+      <View style={styles.buttonContainer}>
+        <Text style={styles.buttonText}>{children}</Text>
+      </View>
+    </Pressable>
+  )
+}
+
+export default PrimaryButton
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    backgroundColor: '#72063c',
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    margin: 4
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
+  }
+})
